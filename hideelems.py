2a90e88d1f3997e358a094cdb0f3d0090a53c42f
Rdast <mirzabekov.rg@yandex.ru>
2023-11-12 20:01:09 +0300

    final
    Основной код

---
+++ hideelems.py
@@ -0,0 +1,24 @@
+import time
+import sys
+import seleniumbase
+from selenium.webdriver.common.by import By
+
+def hideelems(driver: seleniumbase.Driver, viewscompare):
+    try:
+        time.sleep(3)
+        elements = driver.find_elements(By.CLASS_NAME, "i9u")
+        for i in range(len(elements)):
+            elemsspis = elements[i].text.split('\n')
+            views = 0
+            for j in elemsspis:
+                if 'отзыв' in j and 'балл' not in j:
+                    views = int(''.join(j.split('  ')[1].split(' ')[0].split(' ')))
+                    break
+                else:
+                    pass
+            if views < viewscompare:
+                driver.execute_script(f'return document.getElementsByClassName("i9u")[{i}].style.display = "none"')
+    except Exception as e:
+        print("Error:", e)
+        driver.close()
+        sys.exit()
---
+++ main.py
@@ -0,0 +1,65 @@
+import sys
+from hideelems import hideelems
+from seleniumbase import Driver as uc
+from selenium.webdriver.common.keys import Keys as kys
+from selenium.webdriver.common.by import By
+
+
+
+while True:
+    sys.exit()
+    print("Введите какой продукт ищем")
+    inp1 = input()
+    print('Введите от скольки отзывов показывать предложения')
+    inp2 = input()
+    while (not inp2.isdigit()):
+        print('Введено не число. Введите число')
+        inp2 = input()
+    inp2 = int(inp2)
+    pagenow = 1
+    driver = uc(uc=True, browser='Chrome')
+    driver.get("http://www.ozon.ru")
+    while inp1 != 3:
+        try:
+            elem = driver.find_element(By.NAME, "text")
+            elem.send_keys(inp1)
+            elem.send_keys(kys.ENTER)
+            hideelems(driver, inp2)
+            while True:
+                print("1.Предидущия страница.\n2.Следующая страница.\n3.Выход.\nВвод: ", end='')
+                inp1 = input()
+                while (inp1 != '1' and inp1 != '2' and inp1 != '3'):
+                    print('Неправильный ввод. Попробуйте снова.\nВвод: ', end='')
+                    inp1 = input()
+                if inp1 == '1':
+                    if pagenow == 1:
+                        system('cls')
+                        print('Вы и так на первой страничке')
+                    else:
+                        pagenow -= 1
+                        elements = driver.find_elements(By.CLASS_NAME, 'e5q')
+                        for i in elements:
+                            if i.text == str(pagenow):
+                                i.click()
+                                hideelems(driver, inp2)
+                                break
+                elif inp1 == '2':
+                    pagenow += 1
+                    fl = True
+                    elements = driver.find_elements(By.CLASS_NAME, 'e5q')
+                    for i in elements:
+                        if i.text == str(pagenow):
+                            i.click()
+                            hideelems(driver, inp2)
+                            fl = False
+                            break
+                    if fl:
+                        system('cls')
+                        print('Вы и так на последней страничке')
+                elif inp1 == '3':
+                    driver.close()
+                    sys.exit()
+        except Exception as e:
+            print("Error:", e)
+            driver.close()
+            sys.exit()
